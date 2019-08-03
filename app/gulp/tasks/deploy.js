fd5ab9f06de1bbcc8f25e782b9f90947098f16d2
Zachary <zstone@mail.com>
2019-08-03 02:07:02 -0700

    added images

--- app/gulp/tasks/deploy.js
+++ app/gulp/tasks/deploy.js
@@ -6,6 +6,6 @@ var deploy = require('gulp-gh-pages');
  * Push build to gh-pages
  */
 gulp.task('deploy', function () {
-  return gulp.src(["./app/**/*","!./app/ignore/**/*"])
+  return gulp.src(["./app/**/*","!./app/gulp/**/*"])
     .pipe(deploy())
 });
